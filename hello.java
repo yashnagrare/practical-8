16fc947dcb40bd8deb93ce9642748902dbc23bd9
yashnagrare <72175370+yashnagrare@users.noreply.github.com>
2022-05-31 21:28:22 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-public class hello {
- public static void main(String[] args) {
-   system.out.println("hello yash");
-   }
+public class hello
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
